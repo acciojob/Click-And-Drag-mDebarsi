5577526c3a317dbe198dfae8abc291b017b19d87
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-20 20:53:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your code here.
+let isDragging = false;
+let startX, currentX, scrollLeft;
+
+document.addEventListener('mousedown', (e) => {
+  if (e.target.classList.contains('cube')) {
+    isDragging = true;
+    startX = e.clientX;
+    scrollLeft = document.querySelector('.container').scrollLeft;
+  }
+});
+
+document.addEventListener('mousemove', (e) => {
+  if (!isDragging) return;
+  e.preventDefault();
+  currentX = e.clientX;
+  const delta = currentX - startX;
+  document.querySelector('.container').scrollLeft = scrollLeft - delta;
+});
+
+document.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+document.addEventListener('mouseleave', () => {
+  isDragging = false;
+});
